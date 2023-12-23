88537fa0c6fb0a1ab97e45c23607cf71ae867e35
Ishita Chakravorty <ishitachakravorty@Ishitas-Mini.home>
2023-12-23 13:22:34 -0600

    project

---
+++ property_listings_project/src/App.js
@@ -0,0 +1,30 @@
+
+import './App.css';
+import { useContext, createContext, useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Nav from './navigation/Nav';
+import PropertyListing from './components/property/propertyListing';
+
+const NavContext = createContext()
+  export const NavData = () => useContext(NavContext)
+function App() {
+  
+  const [pageName, setPageName] = useState("")
+  useEffect(()=>{
+    setPageName("Property Listings")
+  },[setPageName])
+  return (
+
+   <NavContext.Provider value={{pageName, setPageName}}>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Nav/>}>
+          <Route path="properties" element={<PropertyListing/>}></Route>
+        </Route>
+      </Routes>
+    </Router>
+   </NavContext.Provider>
+  );
+}
+
+export default App;
